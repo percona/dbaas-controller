0afa14ed080f71ed2f4f9947de0592a28b4d794b
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-07-01 22:52:22 +0300

    Add dbaas-api import to track dependency (#8)

--- service/cluster/cluster.go
+++ service/cluster/cluster.go
@@ -17,6 +17,10 @@
 // Package cluster TODO
 package cluster
 
+import (
+	controllerv1beta1 "github.com/percona-platform/dbaas-api/gen/controller"
+)
+
 // TODO: Implement your gRPC server
 
 // Service implements methods of gRPC server and other business logic.
@@ -25,5 +29,8 @@ type Service struct {
 
 // New returns new Service instance.
 func New() *Service {
+	// TODO remove it once it is really used
+	_ = controllerv1beta1.NewXtraDBClusterAPIClient(nil)
+
 	return new(Service)
 }
