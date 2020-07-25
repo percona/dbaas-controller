3713fae708bdbbaa8c6dc5f1a468113887c86b5b
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-07-25 12:09:01 +0300

    PMM-5895 Cleanup kubectl

--- service/k8sclient/kubectl.go
+++ service/k8sclient/kubectl.go
@@ -22,9 +22,10 @@ import (
 	"encoding/json"
 	"os/exec"
 	"strings"
-	"sync"
 
+	"github.com/percona/pmm/utils/pdeathsig"
 	"github.com/pkg/errors"
+	"golang.org/x/sys/unix"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/percona-platform/dbaas-controller/logger"
@@ -33,92 +34,91 @@ import (
 // errNotFound is an error in case of object not found.
 var errNotFound = errors.New("object not found")
 
-// newKubeCtl returns new kubeCtl object.
-func newKubeCtl(l logger.Logger) *kubeCtl {
-	return &kubeCtl{
-		l: l.WithField("component", "kubectl"),
-	}
-}
-
-// kubeCtl is a wrapper for kubectl cli command.
+// kubeCtl wraps kubectl CLI with version selection and kubeconfig handling.
 type kubeCtl struct {
 	l   logger.Logger
 	cmd []string
-
-	once sync.Once
 }
 
-// run runs kubectl commands in cli.
+// newKubeCtl creates a new kubeCtl object with a given logger.
+func newKubeCtl(l logger.Logger) *kubeCtl {
+	// TODO accept and handle version
+	// TODO find correct kubectl binary for given version
+	// TODO accept and handle kubeconfig
+
+	cmd := []string{"dbaas-kubectl-1.16"}
+	path, err := exec.LookPath(cmd[0])
+	l.Debugf("path: %s, err: %v", path, err)
+	if e, ok := err.(*exec.Error); err != nil && ok && e.Err == exec.ErrNotFound {
+		cmd = []string{"minikube", "kubectl", "--"}
+	}
+	l.Debugf("Using %q", strings.Join(cmd, " "))
+
+	return &kubeCtl{
+		l:   l.WithField("component", "kubectl"),
+		cmd: cmd,
+	}
+}
+
+// run executes kubectl with given arguments and stdin data (encoded as JSON),
+// and returns stdout, stderr and execution error.
 func (k *kubeCtl) run(ctx context.Context, args []string, stdin interface{}) ([]byte, []byte, error) {
-	k.once.Do(k.init)
-	var stdOutBuf bytes.Buffer
-	var stdErrBuf bytes.Buffer
 	args = append(k.cmd, args...)
 
-	var buf bytes.Buffer
-	e := json.NewEncoder(&buf)
-	if err := e.Encode(stdin); err != nil {
-		return nil, nil, err
+	var inBuf bytes.Buffer
+	if stdin != nil {
+		e := json.NewEncoder(&inBuf)
+		e.SetIndent("", "  ")
+		if err := e.Encode(stdin); err != nil {
+			return nil, nil, err
+		}
+		k.l.Debugf("Running %s with input:\n%s", strings.Join(args, " "), inBuf.String())
+	} else {
+		k.l.Debugf("Running %s", strings.Join(args, " "))
 	}
-	k.l.Debugf("%v : %s", args, buf.String())
 
-	cmd := exec.CommandContext(ctx, args[0], args[1:]...) // nolint:gosec
-	cmd.Stdin = &buf
-	cmd.Stdout = &stdOutBuf
-	cmd.Stderr = &stdErrBuf
-	defer func() {
-		k.l.Debugf(stdOutBuf.String())
-		k.l.Debugf(stdErrBuf.String())
-	}()
-	if err := cmd.Run(); err != nil {
-		return stdOutBuf.Bytes(), stdErrBuf.Bytes(), err
-	}
-	return stdOutBuf.Bytes(), stdErrBuf.Bytes(), nil
+	var outBuf bytes.Buffer
+	var errBuf bytes.Buffer
+	cmd := exec.CommandContext(ctx, args[0], args[1:]...) //nolint:gosec
+	pdeathsig.Set(cmd, unix.SIGKILL)
+	cmd.Stdin = &inBuf
+	cmd.Stdout = &outBuf
+	cmd.Stderr = &errBuf
+	err := cmd.Run()
+
+	k.l.Debugf(outBuf.String()) // FIXME
+	k.l.Debugf(errBuf.String())
+	return outBuf.Bytes(), errBuf.Bytes(), err
 }
 
-// apply runs kubectl apply command.
+// apply executes `kubectl apply` with given resource.
 func (k *kubeCtl) apply(ctx context.Context, res meta.Object) error {
 	_, _, err := k.run(ctx, []string{"apply", "-f", "-"}, res)
-	if err != nil {
-		return err
-	}
-	return nil
+	return err
 }
 
-// delete runs kubectl delete command.
+// delete executes `kubectl delete` with given resource.
 func (k *kubeCtl) delete(ctx context.Context, res meta.Object) error {
 	_, _, err := k.run(ctx, []string{"delete", "-f", "-"}, res)
-	if err != nil {
-		return err
-	}
-	return nil
+	return err
 }
 
-// get runs kubectl get command and returns `errNotFound` if object not found.
+// get executes `kubectl get` with given object kind and optional name,
+// and decodes resource into `res`.
+// It returns `errNotFound` if object is not found.
 func (k *kubeCtl) get(ctx context.Context, kind string, name string, res interface{}) error {
 	args := []string{"get", "-o=json", kind}
 	if name != "" {
 		args = append(args, name)
 	}
+
 	stdout, stderr, err := k.run(ctx, args, nil)
 	if err != nil {
-		if strings.Contains(string(stderr), "not found") {
+		if strings.Contains(string(stderr), "not found") { // FIXME
 			return errNotFound
 		}
 		return err
 	}
 
 	return json.Unmarshal(stdout, res)
-}
-
-// init selects available kubectl in current system.
-func (k *kubeCtl) init() {
-	cmd := []string{"dbaas-kubectl-1.16"}
-
-	path, err := exec.LookPath("dbaas-kubectl-1.16")
-	k.l.Debugf("path: %s, err: %v", path, err)
-	if e, ok := err.(*exec.Error); err != nil && ok && e.Err == exec.ErrNotFound {
-		cmd = []string{"minikube", "kubectl", "--"}
-	}
-	k.cmd = cmd
 }
