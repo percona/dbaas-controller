8cbddb8b490e31bd3280146585b0df6f8e31ec88
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2020-08-07 17:54:44 +0300

    PMM-6347 Remove blank line

--- service/k8sclient/k8client_test.go
+++ service/k8sclient/k8client_test.go
@@ -23,9 +23,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/percona-platform/dbaas-controller/utils/app"
-
 	"github.com/percona-platform/dbaas-controller/service/k8sclient/kubectl"
+	"github.com/percona-platform/dbaas-controller/utils/app"
 )
 
 func TestK8Client(t *testing.T) {
