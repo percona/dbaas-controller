d0f7c7a96a1fbfc975c6696ea132cb781a14a1ee
Carlos Salguero <carlos.salguero@percona.com>
2020-10-19 15:01:40 -0300

    PMM-6653 Updated license

--- k8_api/pxc/v1/doc.go
+++ k8_api/pxc/v1/doc.go
@@ -1,3 +1,19 @@
+// dbaas-controller
+// Copyright (C) 2020 Percona LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 // Package v1 contains API Schema definitions for the pxc v1 API group
 // +k8s:deepcopy-gen=package,register
 // +groupName=pxc.percona.com
