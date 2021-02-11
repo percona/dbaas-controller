9e5b81de1689fec4006407fef14419fa2368ee86
Jan Prukner <jan.prukner.sde@gmail.com>
2021-02-11 18:00:55 +0100

    PMM-7250 Return NotFound on Get when cluster is deleted, FailedPrecondition when it's not ready, Rename Get -> GetCredentials (#135)
    * PMM-7250 Return NotFound on Get when cluster is deleted
    
    * PMM-7250 Return NotFound on Get when cluster is not Ready
    
    * Avoid dependencies on implementation of k8sclient package
    
    * Add tests for Getting non-Ready cluster
    
    * Get names of clusters back
    
    * Return FailedCondition when cluster is not ready
    
    * Add api change - renaming GetXXX to GetXXXCredentials
    
    * Rename GetXXX to GetXXXCredentials
    
    * use assert.EqualError
    
    * fix EqualError call
    
    * Change err message to reflect whats going on
    
    * move ErrNotFound to the top of the file
    
    * check cause not wrapped err
    
    * use minikube 1.16.0 for tests
    
    * add exec permissions
    
    * remove misplaced comment, cosmetics
    
    * Fix running `make help` (#136)
    
    Currently, it just hangs with `aws configure` waiting for the input
    in the background.
    
    * Bump github.com/percona/pmm (#138)
    
    Bumps [github.com/percona/pmm](https://github.com/percona/pmm) from 2.13.0+incompatible to 2.14.0+incompatible.
    - [Release notes](https://github.com/percona/pmm/releases)
    - [Commits](https://github.com/percona/pmm/compare/v2.13.0...v2.14.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Nurlan Moldomurov <nurlan.moldomurov@percona.com>
    
    * Bump github.com/golangci/golangci-lint from 1.35.2 to 1.36.0 in /tools (#140)
    
    Bumps [github.com/golangci/golangci-lint](https://github.com/golangci/golangci-lint) from 1.35.2 to 1.36.0.
    - [Release notes](https://github.com/golangci/golangci-lint/releases)
    - [Changelog](https://github.com/golangci/golangci-lint/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/golangci/golangci-lint/compare/v1.35.2...v1.36.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Update dependency on dbaas-api
    
    Co-authored-by: Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Nurlan Moldomurov <nurlan.moldomurov@percona.com>

--- service/k8sclient/k8sclient_test.go
+++ service/k8sclient/k8sclient_test.go
@@ -49,6 +49,14 @@ func TestK8sClient(t *testing.T) {
 
 	l := logger.Get(ctx)
 
+	t.Run("Get non-existing clusters", func(t *testing.T) {
+		t.Parallel()
+		_, err := client.GetPSMDBClusterCredentials(ctx, "d0ca1166b638c-psmdb")
+		assert.EqualError(t, errors.Cause(err), ErrNotFound.Error())
+		_, err = client.GetXtraDBClusterCredentials(ctx, "871f766d43f8e-xtradb")
+		assert.EqualError(t, errors.Cause(err), ErrNotFound.Error())
+	})
+
 	pmmPublicAddress := ""
 	t.Run("XtraDB", func(t *testing.T) {
 		name := "test-cluster-xtradb"
@@ -70,6 +78,14 @@ func TestK8sClient(t *testing.T) {
 		require.NoError(t, err)
 
 		l.Info("XtraDB Cluster is created")
+
+		assertListXtraDBCluster(ctx, t, client, name, func(cluster *XtraDBCluster) bool {
+			return cluster != nil
+		})
+		t.Run("Get credentials of cluster that is not Ready", func(t *testing.T) {
+			_, err := client.GetXtraDBClusterCredentials(ctx, name)
+			assert.EqualError(t, errors.Cause(err), ErrXtraDBClusterNotReady.Error())
+		})
 
 		t.Run("Create cluster with the same name", func(t *testing.T) {
 			err = client.CreateXtraDBCluster(ctx, &XtraDBParams{
@@ -148,6 +164,15 @@ func TestK8sClient(t *testing.T) {
 		require.NoError(t, err)
 
 		l.Info("PSMDB Cluster is created")
+
+		assertListPSMDBCluster(ctx, t, client, name, func(cluster *PSMDBCluster) bool {
+			return cluster != nil
+		})
+
+		t.Run("Get credentials of cluster that is not Ready", func(t *testing.T) {
+			_, err := client.GetPSMDBClusterCredentials(ctx, name)
+			assert.EqualError(t, errors.Cause(err), ErrPSMDBClusterNotReady.Error())
+		})
 
 		t.Run("Create cluster with the same name", func(t *testing.T) {
 			err = client.CreatePSMDBCluster(ctx, &PSMDBParams{
--- tools/go.mod
+++ tools/go.mod
@@ -3,7 +3,7 @@ module github.com/percona-platform/dbaas-controller/tools
 go 1.15
 
 require (
-	github.com/golangci/golangci-lint v1.35.2
+	github.com/golangci/golangci-lint v1.36.0
 	github.com/quasilyte/go-consistent v0.0.0-20200404105227-766526bf1e96
 	github.com/reviewdog/reviewdog v0.11.0
 	golang.org/x/text v0.3.5
