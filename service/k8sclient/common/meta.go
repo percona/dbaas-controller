1fc7df8dad27590ece372507a39b2c96ba2ce634
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2021-07-14 01:15:25 +0300

    PMM-8214 Address comments.

--- service/k8sclient/common/meta.go
+++ service/k8sclient/common/meta.go
@@ -83,20 +83,20 @@ type LabelSelector struct {
 	// map is equivalent to an element of matchExpressions, whose key field is "key", the
 	// operator is "In", and the values array contains only "value". The requirements are ANDed.
 	// +optional
-	MatchLabels map[string]string `json:"matchLabels,omitempty" protobuf:"bytes,1,rep,name=matchLabels"`
+	MatchLabels map[string]string `json:"matchLabels,omitempty"`
 	// matchExpressions is a list of label selector requirements. The requirements are ANDed.
 	// +optional
-	MatchExpressions []LabelSelectorRequirement `json:"matchExpressions,omitempty" protobuf:"bytes,2,rep,name=matchExpressions"`
+	MatchExpressions []LabelSelectorRequirement `json:"matchExpressions,omitempty"`
 }
 
 // LabelSelectorRequirement is a selector that contains values, a key, and an operator that
 // relates the key and values.
 type LabelSelectorRequirement struct {
 	// key is the label key that the selector applies to.
-	Key string `json:"key" patchStrategy:"merge" patchMergeKey:"key" protobuf:"bytes,1,opt,name=key"`
+	Key string `json:"key"`
 	// values is an array of string values. If the operator is In or NotIn,
 	// the values array must be non-empty. If the operator is Exists or DoesNotExist,
 	// the values array must be empty. This array is replaced during a strategic
 	// merge patch.
-	Values []string `json:"values,omitempty" protobuf:"bytes,3,rep,name=values"`
+	Values []string `json:"values,omitempty"`
 }
