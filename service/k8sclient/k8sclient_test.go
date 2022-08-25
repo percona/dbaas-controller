41b3ed58fa7911b4098e939d20445a6416ea4558
Andrew Minkin <andrew.minkin@percona.com>
2022-08-25 16:12:11 +0600

    PMM-8432 Fixed tests

--- service/k8sclient/k8sclient_test.go
+++ service/k8sclient/k8sclient_test.go
@@ -1345,7 +1345,7 @@ func getDeploymentCount(ctx context.Context, client *K8sClient, name string) (in
 func printLogs(t *testing.T, ctx context.Context, client *K8sClient, name string) {
 	t.Helper()
 
-	pods, err := client.GetPods(ctx, "-lapp.kubernetes.io/instance="+name)
+	pods, err := client.GetPods(ctx, "app.kubernetes.io/instance="+name)
 	require.NoError(t, err)
 
 	for _, ppod := range pods.Items {
